31fe8fa594776a138a6e1d8131e279ed84c0fb80
pyramid-seph <pyramid-seph@users.noreply.github.com>
2025-05-26 11:37:15 -0600

    Lolbark

--- player.gd
+++ player.gd
@@ -2,4 +2,6 @@ extends CharacterBody2D
 
 
 func _physics_process(delta: float) -> void:
-	pass
+	var vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = vector * 500.0
+	move_and_slide()
